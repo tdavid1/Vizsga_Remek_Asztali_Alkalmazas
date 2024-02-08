c50966eef3cad99ba5bc49268c0ef83685798552
Tomik David <tomik.david1@gmail.com>
2024-02-08 11:24:31 +0100

    Add project files.

---
+++ Vizsga_Remek_Asztali_Alkalmazas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Vizsga_Remek_Asztali_Alkalmazas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
